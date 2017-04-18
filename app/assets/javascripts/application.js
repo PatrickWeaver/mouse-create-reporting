258c645697f77e9c1d396a2caacfa14329a2aba8
Patrick Weaver <pjpwea@gmail.com>
2017-04-18 09:56:06 -0400

    clean up front end for demographic report selection

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,7 +5,120 @@
 //= require chart.2.5.0
 //= require_tree .
 
+
 $(document).ready(function(e) {
+
+  var sel_number = $( "div#demographics > p > select.sel-number" );
+  var sel_scope = $( "div#demographics > p > select.sel-scope" );
+  var div_id = $( "div#demographics > p > span.id" );
+  var div_filter = $( "div#demographics > p > span.filter" );
+  var sel_filter = $( "div#demographics > p > span > select.sel-filter" );
+  var in_id = $( "div#demographics > p > span > input.in-id" );
+  var demog_link_but = $( "div#demographics > p > button.create-link");
+  var new_link = $( "div#demographics > p#link");
+
+  sel_number.change(function(){
+    if (sel_number.val() == "one"){
+      sel_scope.children("option").each(function() {
+        $(this).html($(this).html().slice(0, -1));
+      });
+      div_id.show();
+      sel_filter.val("");
+      div_filter.hide();
+    } else if (sel_number.val() === "all"){
+      sel_scope.children("option").each(function() {
+        $(this).html(String($(this).html()) + "s");
+      });
+      div_id.hide();
+      in_id.val("");
+    }
+  });
+
+  sel_scope.change(function(){
+    if (sel_number.val() === "all" && sel_scope.val() === "site"){
+      div_filter.show();
+      sel_filter.html("<option value=''>all</option><option value='network'>Network</option>");
+    } else if (sel_number.val() === "all" && sel_scope.val() === "group"){
+      div_filter.show();
+      sel_filter.html("<option value=''>all</option><option value='network'>Network</option><option value='site'>Site</option>");
+    } else if (sel_scope.val() === "network"){
+      div_filter.hide();
+      sel_filter.html("");
+      div_id.hide();
+      in_id.val("");
+    }
+  });
+
+  sel_filter.change(function(){
+    if (sel_filter.val() === ""){
+      div_id.hide();
+      in_id.val("");
+    } else {
+      div_id.show();
+    }
+  });
+
+  demog_link_but.click(function() {
+    var link = "/reports/demographics?";
+    scope = "scope=" + sel_scope.val();
+    switch (sel_scope.val()) {
+      case "network":
+        if (sel_number.val() === "one") {
+          filter = "&filter=network";
+          id = "&id=" + in_id.val();
+        } else {
+          filter = "";
+          id = "";
+        }
+      break;
+      case "site":
+        if (sel_number.val() === "one") {
+          filter = "&filter=site";
+          id = "&id=" + in_id.val();
+        } else {
+          if (sel_filter.val() === "network"){
+            filter = "&filter=network";
+            id = "&id=" + in_id.val();
+          } else {
+            filter = "";
+            id = "";
+          }
+        }
+      break;
+      case "group":
+      if (sel_number.val() === "one") {
+        filter = "&filter=group";
+        id = "&id=" + in_id.val();
+      } else {
+        if (sel_filter.val() === "network"){
+          filter = "&filter=network";
+          id = "&id=" + in_id.val();
+        } else if (sel_filter.val() === "site"){
+          filter = "&filter=site";
+          id = "&id=" + in_id.val();
+        } else {
+          filter = "";
+          id = "";
+        }
+      }
+      break;
+    }
+    link += scope + filter + id;
+    new_link.html("<a href='" + link + "'>" + link + "</a>");
+  });
+
+
+
+});
+
+
+
+
+
+
+/*
+
+
   $( "ul#demographics > li > ul > li#scopeNetworkFilterNetworkSingleNetwork > button" ).click(function() {
     id = $( "ul#demographics > li > ul > li#scopeNetworkFilterNetworkSingleNetwork > input" ).val();
     $( "ul#demographics > li > ul > li#scopeNetworkFilterNetworkSingleNetwork > div.report-link").html(
@@ -60,7 +173,7 @@ $(document).ready(function(e) {
 });
 
 
-
+*/
 
 $( function() {
   $( ".datepicker" ).datepicker({
